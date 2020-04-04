50f1c8797be0c82b3be7feb079e4309a9bc7b96b
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-04-04 12:56:47 +0530

    phase1 nav done

--- app/src/main/java/com/example/securex/RegistrationPhaseOneFragment.java
+++ app/src/main/java/com/example/securex/RegistrationPhaseOneFragment.java
@@ -2,17 +2,25 @@ package com.example.securex;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.securex.databinding.FragmentRegistrationPhaseOneBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class RegistrationPhaseOneFragment extends Fragment {
+
+public class RegistrationPhaseOneFragment extends Fragment implements View.OnClickListener {
+
+    private FragmentRegistrationPhaseOneBinding binding;
+
+    private NavController navController;
 
     public RegistrationPhaseOneFragment() {
         // Required empty public constructor
@@ -22,7 +30,34 @@ public class RegistrationPhaseOneFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_phase_one, container, false);
+        binding = FragmentRegistrationPhaseOneBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+        return view;
+   }
+
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        binding.phase1next.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.phase1next:
+                navController.navigate(R.id.action_registrationPhaseOneFragment_to_registrationPhaseTwoFragment);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/securex/RegistrationPhaseTwoFragment.java
+++ app/src/main/java/com/example/securex/RegistrationPhaseTwoFragment.java
@@ -2,15 +2,25 @@ package com.example.securex;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.securex.databinding.FragmentRegistrationPhaseOneBinding;
+import com.example.securex.databinding.FragmentRegistrationPhaseTwoBinding;
 
 
 public class RegistrationPhaseTwoFragment extends Fragment {
+
+    private FragmentRegistrationPhaseTwoBinding binding;
+
+    private NavController navController;
 
     public RegistrationPhaseTwoFragment() {
         // Required empty public constructor
@@ -19,7 +29,16 @@ public class RegistrationPhaseTwoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_phase_two, container, false);
+        binding = FragmentRegistrationPhaseTwoBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+        return view;
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        
     }
 }
