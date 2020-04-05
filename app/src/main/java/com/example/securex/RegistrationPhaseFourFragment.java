f7374c3afe02148f9721b0558ec7c7ab8c1e69c7
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-04-05 15:25:43 +0530

    phase 4 done

--- app/src/main/java/com/example/securex/RegistrationPhaseFourFragment.java
+++ app/src/main/java/com/example/securex/RegistrationPhaseFourFragment.java
@@ -1,16 +1,20 @@
 package com.example.securex;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.example.securex.databinding.FragmentRegistrationPhaseFourBinding;
 import com.example.securex.viewmodel.RegistrationSharedViewModel;
@@ -29,7 +33,7 @@ public class RegistrationPhaseFourFragment extends Fragment implements View.OnCl
     ImageAdapter imageAdapter;
     ArrayList<Integer> FruitsArray;
     ArrayList<Integer> SelectedPositions;
-    private String Password;
+    private String ConfirmPassword;
 
     public RegistrationPhaseFourFragment() {
         // Required empty public constructor
@@ -48,7 +52,27 @@ public class RegistrationPhaseFourFragment extends Fragment implements View.OnCl
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
+        model = new ViewModelProvider(requireActivity()).get(RegistrationSharedViewModel.class);
+        SelectedPositions = new ArrayList<>();
+        setAdapter();
         binding.phase4next.setOnClickListener(this);
+
+        binding.gridview1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                try {
+                    if(!SelectedPositions.contains(position)){
+                        view.setBackgroundColor(getActivity().getResources().getColor(R.color.colorPrimaryDark));
+                        SelectedPositions.add(position);
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        binding.fruitremove2.setOnClickListener(this);
 
 
     }
@@ -58,10 +82,46 @@ public class RegistrationPhaseFourFragment extends Fragment implements View.OnCl
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.phase4next:
-                navController.navigate(R.id.action_registrationPhaseFourFragment_to_registrationPhaseFiveFragment);
-                break;
+                ConfirmPassword = "";
+                int PasswordSize = SelectedPositions.size();
+                for(int i=0;i<SelectedPositions.size();i++){
 
+                    ConfirmPassword+=getActivity().getResources().getResourceEntryName(FruitsArray.get(SelectedPositions.get(i)));
+
+                }
+                if(!ConfirmPassword.equals("")){
+                    if(ConfirmPassword.equals(model.getPassword().getValue().toString())){
+
+                        navController.navigate(R.id.action_registrationPhaseFourFragment_to_registrationPhaseFiveFragment);
+
+                    }
+                    else {
+                        showError();;
+                    }
+                }
+                else showError();
+                  break;
+            case R.id.fruitremove2:
+                if(SelectedPositions.size()>0) {
+                    int end = SelectedPositions.size() - 1;
+                    v = binding.gridview1.getChildAt(SelectedPositions.get(SelectedPositions.size()-1));
+                    SelectedPositions.remove(SelectedPositions.get(SelectedPositions.size() - 1));
+                    v.setBackgroundColor(Color.WHITE);
+                }
+                break;
         }
 
+    }
+    public void setAdapter() {
+        imageAdapter = new ImageAdapter(getActivity().getApplication(),model.getSize().getValue().intValue());
+        binding.gridview1.setAdapter(imageAdapter);
+        getFruitsArray(imageAdapter);
+    }
+    public void getFruitsArray(ImageAdapter imageAdapter) {
+        FruitsArray = imageAdapter.getImageID();
+    }
+
+    public void showError(){
+        Toast.makeText(getActivity().getApplicationContext(),"NOT MATCH",Toast.LENGTH_SHORT).show();
     }
 }
