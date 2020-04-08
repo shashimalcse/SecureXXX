fbebff43478260f003d112b8dbfabb56e685342e
shashimalcse <shashimalsenarath.17@cse.mrt.ac.lk>
2020-04-08 13:46:08 +0530

    logon pin done

--- app/src/main/java/com/example/securex/login/LoginPinFragment.java
+++ app/src/main/java/com/example/securex/login/LoginPinFragment.java
@@ -2,22 +2,67 @@ package com.example.securex.login;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.securex.R;
+import com.example.securex.databinding.FragmentLoginPinBinding;
+import com.example.securex.databinding.FragmentLoginSpinBinding;
+import com.example.securex.viewmodel.LoginSharedViewModel;
+import com.goodiebag.pinview.Pinview;
 
 
 public class LoginPinFragment extends Fragment {
+
+    private FragmentLoginPinBinding binding;
+    private NavController navController;
+    private LoginSharedViewModel model;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_pin, container, false);
+        binding = FragmentLoginPinBinding.inflate(inflater,container,false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        model = new ViewModelProvider(requireActivity()).get(LoginSharedViewModel.class);
+
+        binding.PinUnlock.setPinViewEventListener(new Pinview.PinViewEventListener() {
+            @Override
+            public void onDataEntered(Pinview pinview, boolean fromUser) {
+                if(binding.PinUnlock.getValue().equals(model.getUser(getContext()).getPin())){
+                    showSuccess();
+                }
+                else {
+                    showError();
+                    binding.PinUnlock.clearValue();
+                }
+            }
+        });
+    }
+
+    private void showError() {
+
+        Toast.makeText(getContext(),"NOT MATCH",Toast.LENGTH_SHORT).show();
+    }
+
+    private void showSuccess() {
+
+        Toast.makeText(getContext(),"SUCCESS",Toast.LENGTH_SHORT).show();
     }
 }
