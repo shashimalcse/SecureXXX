f6a70d9a7c0fef80a0861fceb15bbe2f66f1570f
Janaka <janaka.shamal1997@gmail.com>
2020-06-29 16:12:12 +0530

    Testing Added

--- app/src/test/java/com/example/securex/filesecurity/Utils/EncryptorTest.java
+++ app/src/test/java/com/example/securex/filesecurity/Utils/EncryptorTest.java
@@ -19,23 +19,77 @@ import javax.crypto.NoSuchPaddingException;
 import static org.junit.Assert.*;
 
 public class EncryptorTest {
-
+    //test for audio encryption
     @Test
-    public void encryptToFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+    public void encryptToAudioFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
         File f=new File("src/test/resources","test.mp3");
         InputStream is=new FileInputStream(f);
         File out=new File("src/test/resources");
-        File outputFileEnc=new File(out,"EncryptedFile");
+        File outputFileEnc=new File(out,"EncryptedAudioFile");
         Encryptor.encryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
     }
 
     @Test
-    public void decryptToFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
-        File f=new File("src/test/resources","EncryptedFile");
+    public void decryptToAudioFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","EncryptedAudioFile");
         InputStream is=new FileInputStream(f);
-        File outputFileEnc=new File("src/test/resources","test2.mp3");
+        File outputFileEnc=new File("src/test/resources","Decrypted.mp3");
         Encryptor.decryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
     }
+    //test for image encryption
+    @Test
+    public void encryptToImageFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","test.png");
+        InputStream is=new FileInputStream(f);
+        File out=new File("src/test/resources");
+        File outputFileEnc=new File(out,"EncryptedImageFile");
+        Encryptor.encryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
+    }
+
+    @Test
+    public void decryptToImageFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","EncryptedImageFile");
+        InputStream is=new FileInputStream(f);
+        File outputFileEnc=new File("src/test/resources","Decrypted.png");
+        Encryptor.decryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
+    }
+    //test for video encryption
+    @Test
+    public void encryptToVideoFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","test.mp4");
+        InputStream is=new FileInputStream(f);
+        File out=new File("src/test/resources");
+        File outputFileEnc=new File(out,"EncryptedVideoFile");
+        Encryptor.encryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
+    }
+
+    @Test
+    public void decryptToVideoFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","EncryptedVideoFile");
+        InputStream is=new FileInputStream(f);
+        File outputFileEnc=new File("src/test/resources","Decrypted.mp4");
+        Encryptor.decryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
+    }
+
+    //test for text encryption
+    @Test
+    public void encryptToTextFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","test.txt");
+        InputStream is=new FileInputStream(f);
+        File out=new File("src/test/resources");
+        File outputFileEnc=new File(out,"EncryptedTextFile");
+        Encryptor.encryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
+    }
+
+    @Test
+    public void decryptToTextFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","EncryptedTextFile");
+        InputStream is=new FileInputStream(f);
+        File outputFileEnc=new File("src/test/resources","Decrypted.txt");
+        Encryptor.decryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
+    }
+
+    //test for folder encryption
     @Test
     public void encryptToFolder() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
         ZipArchive zipArchive = new ZipArchive();
@@ -54,4 +108,24 @@ public class EncryptorTest {
         Encryptor.decryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
         zipArchive.unzip("src/test/resources/Folder/DecryptedFolder.zip","src/test/resources/Folder","");
     }
+
+    @Test
+    //Test for anyfile encryption
+    public void encryptToAnyFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","test.aep");
+        InputStream is=new FileInputStream(f);
+        File out=new File("src/test/resources");
+        File outputFileEnc=new File(out,"EncryptedAdobeFile");
+        Encryptor.encryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
+    }
+
+    @Test
+    public void decryptToAnyFile() throws IOException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, InvalidKeyException, NoSuchPaddingException {
+        File f=new File("src/test/resources","EncryptedAdobeFile");
+        InputStream is=new FileInputStream(f);
+        File outputFileEnc=new File("src/test/resources","DecryptedAdobeFile.aep");
+        Encryptor.decryptToFile("1111111111111111","1111111111111111",is,new FileOutputStream(outputFileEnc));
+    }
+
+
 }
